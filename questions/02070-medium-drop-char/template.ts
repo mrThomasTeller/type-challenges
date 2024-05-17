e8a020a1a525c2f9979ba888c6aed8f294bba006
Artem Bakharev <artyom.bakharev@gmail.com>
2024-05-17 21:31:49 +0000

    drop-char

--- questions/02070-medium-drop-char/template.ts
+++ questions/02070-medium-drop-char/template.ts
@@ -1,1 +1,13 @@
-type DropChar<S, C> = any
+import type { Call, Strings } from 'hotscript';
+
+type DropChar<S extends string, C extends string> = C extends ''
+  ? never
+  : Call<Strings.Replace<C, '', S>>;
+
+// type DropChar<S extends string, C extends string, _Result extends string = ''> = C extends ''
+//   ? never
+//   : S extends `${infer A}${infer B}`
+//     ? DropChar<B, C, A extends C ? _Result : `${_Result}${A}`>
+//     : _Result;
+
+type dc = DropChar<' b u t t e r f l y ! ', ''>;
